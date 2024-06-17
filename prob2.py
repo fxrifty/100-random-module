d01fd9eaeb2b84e3fd21e86bf3b669a1ca87860d
fxrifty <logan.stevens@gmx.com>
2024-06-17 13:04:36 -0700

    finished

--- prob2.py
+++ prob2.py
@@ -1,18 +1,18 @@
 import random as r
-def suits():
+def cards():
+
  suit=["Diamonds","Spades","Clubs","Hearts"]
  chosen=r.choice(suit)
- print(chosen)
-def cards():
- card=["Ace","1","2","3","5","6","7","8","9","10","Jack","Queen","King"]
- chosen=r.choice(card)
- print(chosen)
+ card=["Ace","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
+ chosen1=r.choice(card)
 
-def hand():
-    cards(),print("Of"),suits()
-   
-    
+ print(chosen1,"Of",chosen)
 
+print("PLAYER 1'S CARDS")
+for i in range(5):  
+ cards()
 
-
-hand()
+print("")
+print("PLAYER 2'S CARDS")
+for i in range(5):  
+ cards()
--- tsk1.py
+++ tsk1.py
@@ -1,16 +1,15 @@
 import random as r
 
+
 ra=r.randint(1,100)
-print(ra)
-guess=int(input("Enter A Number: "))
+guess=int()
 while ra!=guess:
+    guess=int(input("Enter A Number: "))
     if guess < ra:
         print("Too Low")
-        guess = int(input("Enter Another Number: "))
-        if guess > ra:
+    if guess > ra:
             print("Too High") 
-            guess = int(input("Enter number again: "))
-        if guess==ra:
+    if guess==ra:
          print("Correct!")
       
 
